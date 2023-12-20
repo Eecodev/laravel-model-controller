d94457e3adb42dc3370596d25d4541f24940fe19
Eecodev <mnasser.bbk@gmail.com>
2023-12-20 17:26:56 +0100

    card movies printed

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,11 +3,23 @@
 @section('title', 'My Movies')
 
 @section('content')
-
-    <ul>
-        @foreach ($movies as $movie)
-            <li>{{ $movie->title }}</li>
-        @endforeach
-    </ul>
-
+    <main>
+        <section class="container">
+            <h1>Comics</h1>
+            <div class="row">
+                @foreach ($movies as $movie)
+                    <div class="col-12 col-md-4 col-lg-3">
+                        <div class="card">
+                            <img class="dc-img" src="{{$movie->image}}" alt="{{ $movie->title }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$movie->title}}</h5>
+                                <p class="card-title">{{ $movie->original_title }}</p>
+                                <a href="#" class="btn btn-success">Info</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </section>
+    </main>
 @endsection
